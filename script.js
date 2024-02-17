c067b049056803ae9aa189d19430793ec885ce00
russi115 <shamanse2001@gmail.com>
2024-02-17 11:25:44 -0400

    First commit!

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ $( document ).ready(function() {
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
+
+    var audio = document.getElementById("audioPlayer")
+    var btn_audio = document.getElementById("audio_player")
+    var svgs = document.getElementById("svgs")
+    
     
     envelope.click( function() {
         open();
     });
     btn_open.click( function() {
         open();
+        auidoPlay();
     });
     btn_reset.click( function() {
         close();
+        auidoPause();
     });
 
     function open() {
@@ -21,6 +28,17 @@ $( document ).ready(function() {
     function close() {
         envelope.addClass("close")
            .removeClass("open");
+    }
+    function auidoPlay(){
+        audio.play();
+        btn_audio.style.opacity = 1;
+        svgs.style.opacity = 1
+    }
+    function auidoPause(){
+        audio.pause();
+        audio.currentTime = 0;
+        btn_audio.style.opacity = 0;
+        svgs.style.opacity = 0
     }
    
 });
